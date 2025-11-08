5f4605e559cfa4a39677b504114edc75ead00ff5
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-11-07 16:09:53 -0800

    autograd checks

--- examples/torchax_export.py
+++ examples/torchax_export.py
@@ -9,7 +9,7 @@ import torchax
 from jax import config
 from torchax.interop import jax_jit  # noqa: F401
 
-from aoti_mlip.models.mattersim import M3GnetEnergyModel
+from aoti_mlip.models.mattersim import M3GnetEnergyModel, M3GnetModel, M3GnetWrapper
 from aoti_mlip.utils.aoti_tools import model_make_fx, prepare_model_for_compile
 from aoti_mlip.utils.batch_info import get_example_inputs
 
@@ -78,29 +78,82 @@ time_without_jit = (time_end - time_start) / NCALLS
 # Does it only on the whole batch?
 # grads = jax.grad(model_fn, argnums=(0,), allow_int=True)(batch_tuples)
 # print(grads)
+batch_const = tuple(jax.lax.stop_gradient(x) for x in batch_tuples[2:])  # exclude cell (idx 1)
 
 
 @jax.jit
-def energy_fn(atom_pos):
-    rest = tuple(jax.lax.stop_gradient(x) for x in batch_tuples[1:])
-    inputs = (atom_pos,) + rest
+def energy_displacement_fn(positions, strain):
+    A = jnp.eye(3, dtype=strain.dtype) + strain
+    positions = positions @ A
+    cell = jnp.matmul(batch_tuples[1], A)
+    inputs = (positions, cell) + batch_const
     return model_fn(inputs)
 
 
-force_fn = jax.value_and_grad(energy_fn)
-energy, force = force_fn(batch_tuples[0])
-print("Energy:", energy)
-print("Force:", force)
+@jax.jit
+def energy_force_stress_fn(positions, cell):
+    strain = jnp.zeros((3, 3), dtype=cell.dtype)
+    output, grads = jax.value_and_grad(energy_displacement_fn, argnums=(0, 1))(positions, strain)
+    return output, jnp.negative(grads[0]), grads[1] / jnp.linalg.det(cell)
+
+
+print(energy_force_stress_fn(batch_tuples[0], batch_tuples[1]))
 
 jit_time_start = time.perf_counter()
 for _ in range(NCALLS):
-    energy = energy_fn(batch_tuples[0])
+    energy = energy_force_stress_fn(batch_tuples[0], batch_tuples[1])
 jit_time_end = time.perf_counter()
 jit_time_without_jit = (jit_time_end - jit_time_start) / NCALLS
 print("Without JIT time:", time_without_jit, "s")
 print("JIT time:", jit_time_without_jit, "s")
 print("Speedup:", time_without_jit / jit_time_without_jit)
 
-assert torch.allclose(torch_energy, torch.tensor(energy, device=device), atol=1e-5), (
+assert torch.allclose(torch_energy, torch.tensor(energy[0], device=device), atol=1e-5), (
     "Energy mismatch"
 )
+force_jit_time_start = time.perf_counter()
+for _ in range(NCALLS):
+    energy, force, stress = energy_force_stress_fn(batch_tuples[0], batch_tuples[1])
+force_jit_time_end = time.perf_counter()
+force_jit_time = (force_jit_time_end - force_jit_time_start) / NCALLS
+print("Force JIT time:", force_jit_time, "s")
+
+atom_pos_signature = jax.ShapeDtypeStruct(batch_tuples[0].shape, batch_tuples[0].dtype)
+cell_signature = jax.ShapeDtypeStruct(batch_tuples[1].shape, batch_tuples[1].dtype)
+lowered = jax.jit(energy_force_stress_fn).trace(atom_pos_signature, cell_signature).lower()
+compiled = lowered.compile()
+# print(lowered.cost_analysis())
+
+
+model_to_compile = M3GnetModel(
+    model=torch.load(CKPT_PATH, map_location=device, weights_only=True),
+    device=str(device),
+    compute_force=True,
+    compute_stress=True,
+)
+wrapped_model = M3GnetWrapper(model_to_compile)
+model_to_compile = prepare_model_for_compile(wrapped_model, torch.device(device))
+fx_model = model_make_fx(model_to_compile, example_inputs)
+
+print(fx_model(*example_inputs))
+"""JIT with JaxJit
+
+from torchax.interop import jax_jit
+
+model_jitted = jax_jit(model_fn)
+
+print(model_jitted(batch_tuples))
+
+@jax.jit
+def energy_fn(atom_pos, rest):
+    # prevent AD through the rest of the batch
+    rest = tree_map(jax.lax.stop_gradient, rest)
+    return model_jitted((atom_pos,) + rest)
+
+# value and grad w.r.t. atom_pos only
+force_fn = jax.jit(jax.value_and_grad(lambda a, r: jnp.sum(energy_fn(a, r))))
+
+# First call compiles; subsequent calls with same shapes just run
+energy, force = force_fn(batch_tuples[0], batch_tuples[1:])
+
+"""
